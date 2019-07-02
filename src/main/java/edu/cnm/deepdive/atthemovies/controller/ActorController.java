2bb589a8ef7eac9fd154d39734bcfebca5e75af5
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-07-02 09:12:25 -0600

    Added Actor framework and refined Movie, MovieRepository

---
+++ src/main/java/edu/cnm/deepdive/atthemovies/controller/ActorController.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.atthemovies.controller;
+
+//TODO Add annotations, fields, contructors, methods to make this a REST controller,
+// listening at the relative URL "actors".
+public class ActorController {
+
+}
---
+++ src/main/java/edu/cnm/deepdive/atthemovies/model/entity/Actor.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.atthemovies.model.entity;
+
+
+//TODO Add annotations, fields, and methods to make this an Entity class.
+public class Actor {
+
+}
